32bfd20afbe2d68dbb125ba722d9273b349d41ef
einsoft <einstein@einsoft.com.br>
2011-10-20 19:29:56 +0200

    TESTE

---
+++ admin/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MyLinks module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright::  The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license::    http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package::    mylinks
+ * @subpackage:: admin
+ * @since:       2.5.0
+ * @author::     Magic.Shao <magic.shao@gmail.com> - Susheng Yang <ezskyyoung@gmail.com>
+ * @version::	 $Id $
+**/
+
+echo "<div class='center smallsmall italic pad5'></div>";
+
+xoops_cp_footer();
+?>
---
+++ admin/menu.php
@@ -0,0 +1,49 @@
+<?php
+// $Id: menu.php,v 1.3 2004/02/28 01:35:23 mithyt2 Exp $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+$module_handler =& xoops_gethandler('module');
+$xoopsModule =& XoopsModule::getByDirname('news');
+$moduleInfo =& $module_handler->get($xoopsModule->getVar('mid'));
+$pathImageAdmin = $moduleInfo->getInfo('icons32');
+
+// include_once XOOPS_ROOT_PATH.'/modules/news/include/functions.php';
+// $pathImageAdmin = news_getmoduleoption('dirmoduleadmin').'icons/32';
+
+
+$adminmenu = array();
+global $xoopsModule;
+
+$i = 1;
+$adminmenu[$i]["title"] = "Painel de controle";
+$adminmenu[$i]["link"]  = "admin/index.php";
+$adminmenu[$i]["icon"] = '../../'.$pathImageAdmin.'/home.png';
+$i++;
+$adminmenu[$i]["title"] = "Sobre";
+$adminmenu[$i]["link"]  = "admin/about.php";
+$adminmenu[$i]["icon"] = '../../'.$pathImageAdmin.'/about.png';
+
+?>
